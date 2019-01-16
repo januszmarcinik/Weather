8ed726dfc353b6ed4e9880b39b301b18d5ca209e
Janusz Marcinik <januszmarcinik@gmail.com>
2019-01-16 18:13:27 +0100

    Changed DI container to Autofac

---
+++ Weather.WebUI/App_Start/AutofacConfig.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using Autofac;
+using Autofac.Integration.Mvc;
+using Autofac.Integration.WebApi;
+using Hangfire;
+using Weather.Domain.Abstract;
+using Weather.Domain.Concrete;
+using Weather.WebUI.Jobs;
+using Weather.WebUI.Services;
+
+namespace Weather.WebUI
+{
+    public class AutofacConfig
+    {
+        public static void RegisterServices()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+            builder.RegisterAssemblyModules(typeof(MvcApplication).Assembly);
+
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            builder.RegisterType<OpenWeatherMapService>().As<IExternalApiWeatherService>().InstancePerLifetimeScope();
+            builder.RegisterType<EFCityRepository>().As<ICityRepository>().InstancePerLifetimeScope();
+            builder.RegisterType<EFWeatherRepository>().As<IWeatherRepository>().InstancePerLifetimeScope();
+
+            builder.RegisterType<ReadActualWeatherForAllCitiesJob>();
+            builder.RegisterType<ReadActualWeatherForOneCityJob>();
+
+            var container = builder.Build();
+
+            System.Web.Mvc.DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+            System.Web.Http.GlobalConfiguration.Configuration.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+            GlobalConfiguration.Configuration.UseActivator(new AutofacJobActivator(container));
+        }
+    }
+}
--- Weather.WebUI/Global.asax.cs
+++ Weather.WebUI/Global.asax.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Routing;
 
@@ -11,7 +8,9 @@ namespace Weather.WebUI
     {
         protected void Application_Start()
         {
+            AutofacConfig.RegisterServices();
             AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
     }
